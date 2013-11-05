2ece51f98556666d52dfac8cd2d68c725c91b6f1
liaesque <liaesque@gmail.com>
2013-11-05 11:49:54 -0800

    Included a scroll function to Customs
    When user completes a section, they are automatically lead to the next
    section.

--- Week 9/html5-site/js/custom-order.js
+++ Week 9/html5-site/js/custom-order.js
@@ -1,10 +1,11 @@
 $(document).ready( function() {
 
+$(".otherBox").hide();
 
 	$("#forWho").change( function() {  
 
 		var fadeSpeed = 500;
-		$(".otherBox").hide();
+		
 
 		if($("#forWho").val()=="other") {
 			$(".otherBox").fadeToggle(fadeSpeed);
@@ -15,5 +16,48 @@ $(document).ready( function() {
 		}
 
 	});
+
+	// When user selects something in the "Who is it for" section, 
+	// scroll to next section
+	$("#forWho").change( function() { 
+
+		if($('#forWho').val()=="friend") {
+			$('html, body').animate({scrollTop:$('#what').position().top}, 'slow');
+		}
+
+		else if($('#forWho').val()=="family") {
+			$('html, body').animate({scrollTop:$('#what').position().top}, 'slow');
+		}
+
+		else if($('#forWho').val()=="myself") {
+			$('html, body').animate({scrollTop:$('#what').position().top}, 'slow');
+		}
+	});
+
+	$("#otherNext").click( function() {
+
+		$('html, body').animate({scrollTop:$('#what').position().top}, 'slow');
+
+	});
+
+
+	// When user selects something in the "Type of Jewelry" section, 
+	// scroll to next section
+	$(".jwl").click( function() {
+
+		$('html, body').animate({scrollTop:$('#why').position().top}, 'slow');
+
+	});
+
+
+
+	// After user types something in the "Story" section, 
+	// scroll to next section
+	$("#storyNext").click( function() {
+
+		$('html, body').animate({scrollTop:$('#contact-info').position().top}, 'slow');
+
+	});
+
 
 });
