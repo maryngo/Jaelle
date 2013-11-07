03870dfc819d8e1a9223ce798d9f67dde3df4c25
liaesque <liaesque@gmail.com>
2013-11-07 05:58:28 -0800

    Added Custom page and deleted stuff

---
+++ Week 10/js/custom-order.js
@@ -0,0 +1,63 @@
+$(document).ready( function() {
+
+$(".otherBox").hide();
+
+	$("#forWho").change( function() {  
+
+		var fadeSpeed = 500;
+		
+
+		if($("#forWho").val()=="other") {
+			$(".otherBox").fadeToggle(fadeSpeed);
+		}
+
+		else {
+			$(".otherBox").hide();
+		}
+
+	});
+
+	// When user selects something in the "Who is it for" section, 
+	// scroll to next section
+	$("#forWho").change( function() { 
+
+		if($('#forWho').val()=="friend") {
+			$('html, body').animate({scrollTop:$('#what').position().top + 260}, 'slow');
+		}
+
+		else if($('#forWho').val()=="family") {
+			$('html, body').animate({scrollTop:$('#what').position().top + 260}, 'slow');
+		}
+
+		else if($('#forWho').val()=="myself") {
+			$('html, body').animate({scrollTop:$('#what').position().top + 260}, 'slow');
+		}
+	});
+
+	$("#otherNext").click( function() {
+
+		$('html, body').animate({scrollTop:$('#what').position().top + 260}, 'slow');
+
+	});
+
+
+	// When user selects something in the "Type of Jewelry" section, 
+	// scroll to next section
+	$(".jwl").click( function() {
+
+		$('html, body').animate({scrollTop:$('#why').position().top + 260}, 'slow');
+
+	});
+
+
+
+	// After user types something in the "Story" section, 
+	// scroll to next section
+	$("#storyNext").click( function() {
+
+		$('html, body').animate({scrollTop:$('#contact-info').position().top + 260}, 'slow');
+
+	});
+
+
+});
