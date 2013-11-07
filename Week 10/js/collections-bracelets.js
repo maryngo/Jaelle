6d1947c36f3aae0d1b44f1d2bd8cd86a9d84e1e2
lilianho <lha15@sfu.ca>
2013-11-06 20:15:00 -0800

    made week 10 folder

---
+++ Week 10/js/collections-bracelets.js
@@ -0,0 +1,81 @@
+$(document).ready( function() {
+
+
+$("#price").change( function() {  
+
+	var fadeSpeed = 500;
+
+	// var priceSort = $("#price").val();
+	// $("h1").html(priceSort);
+
+	if($('#price').val()=="10") {
+		$(".price_10").fadeIn(fadeSpeed);
+		$(".price_50").fadeOut(fadeSpeed);
+		$(".price_100").fadeOut(fadeSpeed);
+		$(".bigmoney").fadeOut(fadeSpeed);
+	}
+
+	else if($('#price').val()=="50") {
+		$(".price_10").fadeIn(fadeSpeed);
+		$(".price_50").fadeIn(fadeSpeed);
+		$(".price_100").fadeOut(fadeSpeed);
+		$(".bigmoney").fadeOut(fadeSpeed);
+	}
+
+	else if($('#price').val()=="100") {
+		$(".price_10").fadeIn(fadeSpeed);
+		$(".price_50").fadeIn(fadeSpeed);
+		$(".price_100").fadeIn(fadeSpeed);
+		$(".bigmoney").fadeOut(fadeSpeed);
+	}
+
+	else {
+		$(".bigmoney").fadeIn(fadeSpeed);
+		$(".price_10").fadeIn(fadeSpeed);
+		$(".price_50").fadeIn(fadeSpeed);
+		$(".price_100").fadeIn(fadeSpeed);
+	}
+
+});
+
+// END STUFF
+
+
+	// $(".default-price").click( function() {
+
+	// 	$(".bigmoney").fadeIn(1000);
+	// 	$(".price_10").fadeIn(1000);
+	// 	$(".price_50").fadeIn(1000);
+	// 	$(".price_100").fadeIn(1000);
+
+
+	// });
+
+	// $(".und_100").click( function() {
+
+	// 	$(".price_10").fadeIn(1000);
+	// 	$(".price_50").fadeIn(1000);
+	// 	$(".price_100").fadeIn(1000);
+	// 	$(".bigmoney").fadeOut(1000);
+
+	// });
+
+	// $(".und_50").click( function() {
+
+	// 	$(".price_10").fadeIn(1000);
+	// 	$(".price_50").fadeIn(1000);
+	// 	$(".bigmoney").fadeOut(1000);
+	// 	$(".price_100").fadeOut(1000);
+		
+	// });
+
+	// $(".und_10").click( function() {
+
+	// 	$(".price_10").fadeIn(1000);
+	// 	$(".bigmoney").fadeOut(1000);
+	// 	$(".price_100").fadeOut(1000);
+	// 	$(".price_50").fadeOut(1000);
+
+	// });
+	
+});
---
+++ Week 10/js/main.js
@@ -0,0 +1,116 @@
+$(document).ready( function() {
+
+
+	//cache nav  
+	var nav = $("#topNav");  
+                  
+	//add indicators and hovers to submenu parents  
+	nav.find("li").each(function() {
+
+		if ($(this).find("ul").length > 0) {  
+                  
+			$("<span>").text(" ").appendTo($(this).children(":first"));  
+
+			//show subnav on hover  
+			$(this).mouseenter(function() {  
+				$(this).find("ul").stop(true, true).slideDown();  
+			});  
+                          
+			//hide submenus on exit  
+			$(this).mouseleave(function() {  
+				$(this).find("ul").stop(true, true).slideUp();  
+			});  
+		}
+
+	});  
+
+
+	// Code for Pop-up search
+	$(".searchBox").hide();
+
+	$(".search-button").click( function() {
+
+		$(".searchBox").fadeToggle(500);
+		$(".cartBox").hide();
+
+	});
+
+	// Code for Pop-up cart
+	$(".cartBox").hide();
+
+	$(".cart-button").click( function() {
+
+		$(".cartBox").fadeToggle(500);
+		$(".searchBox").hide();
+
+	});
+
+	// Code for nav
+	if ($(window).width() > 600) {
+		
+		$(".primary").show();
+		$(".secondary").hide();
+
+		$(".drop").mouseenter( function() {
+
+			$(".secondary").show();
+
+		});
+
+		$(".drop").mouseleave( function() {
+
+			$(".secondary").hide();
+		});
+	}
+
+	$(window).resize(function(){
+		if ($(window).width() > 600){
+
+		$(".primary").show();
+		$(".secondary").hide();
+
+		$(".drop").mouseenter( function() {
+
+			$(".secondary").show();
+
+		});
+
+		$(".drop").mouseleave( function() {
+
+			$(".secondary").hide();
+		});
+	}
+	});
+
+	//media query
+	if ($(window).width() <= 600) {
+		
+		$(".primary").hide();
+		$(".secondary").hide();
+
+		$("#nav-icon").click( function() {
+
+			$(".primary").fadeToggle(500);
+			$(".secondary").fadeToggle(500);
+
+		});
+	}
+	
+
+	$(window).resize(function(){
+		if ($(window).width() <= 600){	
+
+		$(".primary").hide();
+		$(".secondary").hide();
+
+		$("#nav-icon").click( function() {
+
+			$(".primary").fadeToggle(500);
+			$(".secondary").fadeToggle(500);
+
+		});
+			
+		}
+	});
+	
+});
