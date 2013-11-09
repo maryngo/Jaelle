a771883fd56938a60a1af35a3dc767d9dca61ecb
liaesque <liaesque@gmail.com>
2013-11-09 02:02:58 -0800

    Fixed Product Detail Gallery
    Highlights the current image thumbnail and took out the "Quatity"
    section.

---
+++ Week 10/js/product-detail.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+	$(".eventHover a").click(function(thumb) {
+
+		thumb.preventDefault();
+
+		//remove the highlight on previous selected item
+		$(".select").removeClass("selected");
+		
+		//place the highlight on the selected the current item
+		$(".select", this).addClass("selected");
+
+	});
+
+
+	
+});
